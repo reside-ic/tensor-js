c77fe074a987b08688141ce867a3426055e31c4f
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-07-20 17:28:19 +0100

    Start thinking about matrix

--- src/matrix.ts
+++ src/matrix.ts
@@ -3,27 +3,53 @@ import {Tensor} from "./tensor";
 
 export class Matrix<T> extends Tensor<T> {
     private readonly nr: number;
+    private readonly nc: number;
     constructor(data: T[], nr: number, nc: number) {
         super(data, [nr, nc]);
         this.nr = nr;
+        this.nc = nc;
     }
 
     public get(i: number, j: number) {
         return this.data[i + j * this.nr];
     }
 
+    public getSafely(i: number, j: number) {
+        this.checkIndex(i, j);
+        return this.get(i, j);
+    }
+
     public set(value: T, i: number, j: number) {
         this.data[i + j * this.nr] = value;
     }
 
-    // public toArray(): T[][] {
-    //     const ret = [];
-    //     for (let i = 0; i < this.nc; ++i) {
-    //     }
-    // }
+    public setSafely(value: T, i: number, j: number) {
+        this.checkIndex(i, j);
+        this.data[i + j * this.nr] = value;
+    }
+    
+    public toArray(mode: boolean): T[][] {
+        const ret = [];
+        if (mode) {
+            for (let i = 0, j = 0; i < this.nr; ++i, j += this.nc) {
+                ret.push(this.data.slice(j, j + this.nc));
+            }
+        } else {
+            for (let i = 0, j = 0; i < this.nc; ++i, j += this.nr) {
+                ret.push(this.data.slice(j, j + this.nr));
+            }
+        }
+        return ret;
+    }
 
     public copyData(): T[] {
         return this.data.slice();
+    }
+
+    private checkIndex(i: number, j: number) {
+        if (i >= this.nr || j >= this.nc) {
+            throw Error("Out of bounds");
+        }        
     }
 
     // public col(j: number) {
--- test/matrix.test.ts
+++ test/matrix.test.ts
@@ -1,18 +1,38 @@
-import {Matrix} from "../src/matrix";
+import { Matrix } from "../src/matrix";
+import { seq, zeros } from "../src/util";
 
 describe("matrix", () => {
     it("can construct a matrix of numbers", () => {
-        const m = new Matrix(0, 3, 5);
+        const m = new Matrix(zeros(15), 3, 5);
         expect(m.dim).toEqual([3, 5]);
-        expect(m.data).toEqual(Array(15).fill(0));
-        console.log(m);
+        expect(m.data).toEqual(zeros(15));
     });
-});
 
-describe("Float64Array usage", () => {
-    it("Can be constructed at all", () => {
-        const data = new Float64Array(15);
-        const m = new Matrix(data, 3, 5);
-        console.log(m);
+    it("can extract elements", () => {
+        const m = new Matrix(seq(15), 3, 5);
+        expect(m.get(0, 0)).toBe(0);
+        //
+        //      [,0] [,1] [,2] [,3] [,4]
+        // [0,]    0    3    6    9   12
+        // [1,]    1    4    7   10   13
+        // [2,]    2    5    8   11   14
+        //
+        // The behaviour here depends critically on how we store
+        // things...
+        expect(m.get(2, 3)).toBe(11);
+        expect(m.get(1, 2)).toBe(7);
+
+        expect(() => m.getSafely(3, 2)).toThrow("Out of bounds");
+    });
+
+    it("can be converted to array-of-arrays", () => {
+        const m = new Matrix(seq(15), 3, 5);
+        const res0 = m.toArray(false);
+        const res1 = m.toArray(true);
+        console.log(res0);
+        console.log(res1);
+
+        expect(res0[3][2]).toBe(m.get(2, 3));
+        expect(res0[2][1]).toBe(m.get(1, 2));
     });
 });
